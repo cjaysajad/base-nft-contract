1956c5af3f7523c19963006b341b6ae9b33677fd
sajad <95574714+cjaysajad@users.noreply.github.com>
2026-08-03 03:18:22 +1000

    Update SimpleNFT.sol

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
 
+pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
